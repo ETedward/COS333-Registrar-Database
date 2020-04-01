1942529aa7f4b0a6efaf2c8eceb318067937686d
Edward Tian <edwardtian2000@gmail.com>
2020-03-31 23:49:14 -0400

    adding input handling and GET request

--- Registrar_Flask/app.py
+++ Registrar_Flask/app.py
@@ -6,8 +6,7 @@
 #-----------------------------------------------------------------------
 
 from sys import argv
-from database import Database
-from time import localtime, asctime, strftime
+#from database import Database
 from flask import Flask, request, make_response, redirect, url_for
 from flask import render_template
 
@@ -17,72 +16,23 @@ app = Flask(__name__, template_folder='.')
 
 #-----------------------------------------------------------------------
 
-def getReg(argv):
-    if len(argv) != 3:
-        print('Usage: python %s ERROR MSG' % argv[0])
-        exit(1)
-
-    regdetails = CALL_REG_FUNCTION (argv)
-    return regdetails
-
-#-----------------------------------------------------------------------
-
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
- 
+    Dept = request.args.get('Dept')
+    Numb = request.args.get('Numb')
+    Area = request.args.get('Area')
+    Title = request.args.get('Title')
+
     html = render_template('index.html',
-        var=getReg(argv))
+                           Dept=Dept,
+                           Numb=Numb,
+                           Area=Area,
+                           Title=Title)
     response = make_response(html)
     return response
     
-#-----------------------------------------------------------------------
-
-@app.route('/searchform', methods=['GET'])
-def searchForm():
-
-    errorMsg = request.args.get('errorMsg')
-    if errorMsg is None:
-        errorMsg = ''
-    
-    prevAuthor = request.cookies.get('prevAuthor')
-    if prevAuthor is None:
-        prevAuthor = '(None)'
-    
-    html = render_template('searchform.html',
-        ampm=getAmPm(),
-        currentTime=getCurrentTime(),
-        errorMsg=errorMsg,
-        prevAuthor=prevAuthor)
-    response = make_response(html)
-    return response    
-    
-#-----------------------------------------------------------------------
-
-@app.route('/searchresults', methods=['GET'])
-def searchResults():
-    
-    author = request.args.get('author')
-    if (author is None) or (author.strip() == ''):
-        errorMsg = 'Please type an author name.'
-        return redirect(url_for('searchForm', errorMsg=errorMsg))
- 
-    database = Database()
-    database.connect()
-    books = database.search(author)
-    database.disconnect()
-     
-    html = render_template('searchresults.html',
-        ampm=getAmPm(),
-        currentTime=getCurrentTime(),
-        author=author,
-        books=books)
-    response = make_response(html)
-    response.set_cookie('prevAuthor', author)
-    return response         
-    
-#-----------------------------------------------------------------------
-
+#---------------------------------------------------
 if __name__ == '__main__':
     if len(argv) != 2:
         print('Usage: ' + argv[0] + ' port')
